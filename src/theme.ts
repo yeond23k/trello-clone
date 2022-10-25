d0a3afcfe0a15f681c6f34900c0f467f87d7d671
Daryl <dyk0203@naver.com>
2022-10-26 00:54:36 +0900

    3. Styles and Placeholders

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
+  bgColor: "#4794FA",
+  boardColor: "#E6EAEE",
+  cardColor: "white",
 };
